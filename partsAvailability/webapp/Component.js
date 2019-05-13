3f0116008e5a5846b75e0b05994c9482c0c611f3
Anthony Wat <anthony_wat@toyota.ca>
2019-05-13 12:40:01 -0400

    Added XHR header to OData model def in manifest.json

--- partsAvailability/webapp/Component.js
+++ partsAvailability/webapp/Component.js
@@ -1,9 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-		"sap/ui/model/odata/v2/ODataModel",
+	"sap/ui/model/odata/v2/ODataModel",
 	"partsAvailability/model/models"
-], function(UIComponent, Device, ODataModel, models) {
+], function (UIComponent, Device, ODataModel, models) {
 	"use strict";
 
 	return UIComponent.extend("partsAvailability.Component", {
@@ -11,69 +11,72 @@ sap.ui.define([
 		metadata: {
 			manifest: "json",
 			config: {
-			fullWidth: true 
-		}
+				fullWidth: true
+			}
 		},
-
 
 		/**
 		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
 		 * @public
 		 * @override
 		 */
-		init: function() {
+		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
-		
-		
-// ============================== md_product service==========================Begin
+
+			// ============================== md_product service==========================Begin
 			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/MD_PRODUCT_FS_SRV");
 			//  if running on a local version,  use the destination otherwise use /node.			
 			var sLocation = window.location.host;
 			var sLocation_conf = sLocation.search("webide");
 			if (sLocation_conf == 0) {
 				mConfig.uri = "/partsavailability-node" + mConfig.uri;
-			} else {
-			}
+			} else {}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
-				json: true
+				json: true,
+				headers: {
+					"X-Requested-With": "XMLHttpRequest"
+				}
 			});
 			this.setModel(oDataModel, "materialDisplayModeloData");
-// ============================== md_product service==========================End	
-// ============================== zmd_product service==========================Begin
-				var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZMD_PRODUCT_FS_V2_SRV");
+			// ============================== md_product service==========================End	
+			// ============================== zmd_product service==========================Begin
+			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/ZMD_PRODUCT_FS_V2_SRV");
 			//  if running on a local version,  use the destination otherwise use /node.			
 			var sLocation = window.location.host;
 			var sLocation_conf = sLocation.search("webide");
 			if (sLocation_conf == 0) {
 				mConfig.uri = "/partsavailability-node" + mConfig.uri;
-			} else {
-			}
+			} else {}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
-				json: true
+				json: true,
+				headers: {
+					"X-Requested-With": "XMLHttpRequest"
+				}
 			});
-			this.setModel(oDataModel, "zMaterialDisplayModel");		
-// ============================== zmd_product service==========================Begin
-// ==============================api_business partner==========================Begin
-				var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/API_BUSINESS_PARTNER");
+			this.setModel(oDataModel, "zMaterialDisplayModel");
+			// ============================== zmd_product service==========================Begin
+			// ==============================api_business partner==========================Begin
+			var mConfig = this.getMetadata().getManifestEntry("/sap.app/dataSources/API_BUSINESS_PARTNER");
 			//  if running on a local version,  use the destination otherwise use /node.			
 			var sLocation = window.location.host;
 			var sLocation_conf = sLocation.search("webide");
 			if (sLocation_conf == 0) {
 				mConfig.uri = "/partsavailability-node" + mConfig.uri;
-			} else {
-			}
+			} else {}
 			var oDataModel = new ODataModel(mConfig.uri, {
 				useBatch: false,
-				json: true
+				json: true,
+				headers: {
+					"X-Requested-With": "XMLHttpRequest"
+				}
 			});
-			this.setModel(oDataModel, "aPiBusinessPartner");	
-
+			this.setModel(oDataModel, "aPiBusinessPartner");
 
 		}
 	});
